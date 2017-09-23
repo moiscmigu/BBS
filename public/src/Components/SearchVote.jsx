69a5d206f0bd23400cca97134d54039241353ec8
Moises Miguel <moisesmigueledu@gmail.com>
2017-09-23 12:51:23 -0500

    .

--- public/src/Components/SearchVote.jsx
+++ public/src/Components/SearchVote.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {userSearch} from '../Actions/index';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import swal from 'bootstrap-sweetalert';
 
 
 
@@ -14,16 +15,66 @@ class SearchVote extends React.Component {
         super(props)
 
         this.state = {
-            userVoteAccessToken:''
+            userVoteAccessToken:'',
+            vote:{},
+            handleCardNum:''
+
         };//end of this state
 
 
     }//end of contructor
 
     handleVoteSearch() {
-        console.log('this is the vote the user searched for', this)
+
         this.props.userSearch(this.state.userVoteAccessToken)
     }//end of handleVoteSearch
+
+    handleCategory() {  
+        //changess state
+        console.log('clicked', this.state);
+
+
+
+    }//end of handleCategoryr
+
+    NumberOfVoteCards() {
+        let num = []
+
+        for (var i=0; i < Number(this.state.handleCardNum) + 1; i++) {
+            num.push(i);
+        }
+
+        //handles how many card blocks should be displayed
+        if(num.length === 0) {
+            return false;
+
+        }else {
+
+            return(  
+                <div>
+                    {
+                        num.map((card, id) => {
+                            return (
+                                <div className="card-block" key={id}>
+                                    <input id={id}  type="text" onChange={event => {this.value = event.target.value}}  onClick={this.handleVal(document.getElementById(id))}  />
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            ); 
+        }
+
+               
+       
+
+     
+
+    }//end of handleCardBlock
+
+    handleVal(id) {
+        console.log('handle vbal', id)
+    }
 
 
     render() {
@@ -51,28 +102,35 @@ class SearchVote extends React.Component {
                     </form>
                 </div>
                 <div className="col-md-4 ">
-                <button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-                
 
-                <div id="myModal" className="modal fade" role="dialog">
-                  <div className="modal-dialog">
-                
+                    {/* <input className="btn btn-primary" type="button" value="Start New Vote" data-toggle="modal" data-target="#myModal" onClick={this.setState({voteCategory:prompt('Category Type?')})} /> */}
+                    <button className="btn btn-primary" type="submit" data-toggle="modal" data-target="#myModal" >Start New Vote</button>
 
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal">&times;</button>
-                        <h4 className="modal-title">Modal Header</h4>
-                      </div>
-                      <div className="modal-body">
-                        <p>Some text in the modal.</p>
-                      </div>
-                      <div className="modal-footer">
-                        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                      </div>
+                    <div id="myModal" className="modal fade" role="dialog">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                    <h4 className="modal-title" >Start New Vote</h4>
+                                </div>
+                                <div className="modal-body">
+
+                                <div className="card" >
+                                    <input type='text' onChange={event => this.setState({vote:{...this.state.vote, title:event.target.value}})} placeholder='Title'/>        
+                                    <input type='text' onChange={event => this.setState({vote:{...this.state.vote, voteNum:Number(event.target.value)}})} placeholder='number'/>        
+                                    <input type='text' onChange={event => this.setState({vote:{...this.state.vote, why:event.target.value}})} placeholder='number'/>        
+                                    <button onClick={this.handleCategory.bind(this)} >Enter</button>
+                                </div>
+
+                                   
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+                                </div>
+                            </div>
+                        
+                        </div>
                     </div>
-                
-                  </div>
-                </div>
                 </div>
                 </div>
         </div>
